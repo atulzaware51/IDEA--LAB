6370b653778cb66ce2275d589b59064d9acdbdb5
atulzaware51 <atulzaware1411@gmail.com>
2025-10-05 20:55:00 +0530

    Create inheritance.cpp

---
+++ inheritance.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+
+class Animal {
+public:
+    void eat() {
+        cout << "Eating..." << endl;
+    }
+};
+
+
+class Dog : public Animal {
+public:
+    void bark() {
+        cout << "Woof!" << endl;
+    }
+};
+
+int main() {
+    Dog myDog;
+    myDog.eat(); 
+    myDog.bark();
+    return 0;
+}
