46815bdc40ebb327159e13e54836511ecf4f9c8f
atulzaware51 <atulzaware1411@gmail.com>
2025-10-05 20:57:17 +0530

    Update find prime.c

--- find prime.c
+++ find prime.c
@@ -13,4 +13,9 @@ int main() {
     printf("Enter a number: ");
     scanf("%d", &num);
     if (isPrime(num)) {
-        printf("%d is a prime number.\n
+        printf("%d is a prime number.\n", num);
+    } else {
+        printf("%d is not a prime number.\n", num);
+    }
+    return 0;
+}
